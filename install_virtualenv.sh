50e99b62b1414428bf64f0c93ae582cb2f7cbe68
AlexanderCollins <alexcollins010@gmail.com>
2017-07-06 00:46:35 +1000

    install gunicorn into virtualenv

--- install_virtualenv.sh
+++ install_virtualenv.sh
@@ -3,5 +3,10 @@ sudo apt-get install python3-pip -y
 sudo pip3 install virtualenv
 cd /home/$1/
 virtualenv --no-site-packages -p $(which python3) env
+
 # install mysql client
 sudo /home/$1/env/bin/pip3 install mysqlclient
+
+# install gunicorn
+sudo /home/$1/env/bin/pip3 install gunicorn
+
