2af06876ebeb20bcf5a73c6e573d7d2d33f48906
AlexanderCollins <alexcollins010@gmail.com>
2017-07-05 18:53:12 +1000

    removing debug statements from install_virtualenv

--- install_virtualenv.sh
+++ install_virtualenv.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
-echo "\n\napt-get install python3-pip -y\n\n"
 sudo apt-get install python3-pip -y
-
-echo "\n\nsudo pip3 install virtualenv\n\n"
 sudo pip3 install virtualenv
 cd /home/$1/
-
-echo "\n\nvirtualenv --no-site-packages -p $(which python3) env\n\n"
 virtualenv --no-site-packages -p $(which python3) env
-
 # install mysql client
 sudo /home/$1/env/bin/pip3 install mysqlclient
