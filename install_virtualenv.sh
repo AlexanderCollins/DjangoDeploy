734b488dc8237129ca30e37315ced98bb705d3c2
AlexanderCollins <alexcollins010@gmail.com>
2017-07-05 18:52:12 +1000

    debugging install virtualenv

--- install_virtualenv.sh
+++ install_virtualenv.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+echo "\n\napt-get install python3-pip -y\n\n"
 sudo apt-get install python3-pip -y
-sudo pip3 install virtualenv
 
-cd ~/
+echo "\n\nsudo pip3 install virtualenv\n\n"
+sudo pip3 install virtualenv
+cd /home/$1/
+
+echo "\n\nvirtualenv --no-site-packages -p $(which python3) env\n\n"
 virtualenv --no-site-packages -p $(which python3) env
 
 # install mysql client
-sudo su
-/home/$1/env/bin/pip3 install mysqlclient
-
-exit
-
+sudo /home/$1/env/bin/pip3 install mysqlclient
