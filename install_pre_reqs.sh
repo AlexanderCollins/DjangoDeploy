68bbb5a43fd95653e34e8e6ecefb59a0e1f73255
Alex <alexcollins010@gmail.com>
2017-07-05 18:36:56 +1000

    adding update and upgrade to install pre reqs

--- install_pre_reqs.sh
+++ install_pre_reqs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 #install prereqs
+sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install build-essential git htop -y
 sudo apt-get install python3-dev libmysqlclient-dev -y
